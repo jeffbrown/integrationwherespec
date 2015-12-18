afc518b9d5abd6d7bb2a1a688bef8a62d3be1a5a
Jeff Scott Brown <brownj@ociweb.com>
2015-12-18 10:08:42 -0600

    see https://github.com/grails/grails-core/issues/9493

---
+++ src/integration-test/groovy/demo/DemoSpec.groovy
@@ -0,0 +1,26 @@
+```
+// src/integration-test/groovy/demo/DemoSpec.groovy
+package demo
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.Rollback
+import spock.lang.Specification
+
+@Integration
+class DemoSpec extends Specification {
+
+    @Rollback
+	def "test toUpperCase"() {
+		given:
+		def result = value.toUpperCase()
+
+		expect:
+		result == expectedResult
+
+		where:
+        value          | expectedResult
+        'King Crimson' | 'KING CRIMSON'
+        'Riverside'    | 'RIVERSIDE'
+	}
+}
+```
